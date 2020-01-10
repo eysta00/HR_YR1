43794d2bd0080101ef31b0763faf5561c21848aa
eysta00 <eysi@live.com>
2020-01-10 11:10:49 +0000

    line_test.py

---
+++ Test Code/line_test.py
@@ -0,0 +1,6 @@
+single_line1 = "Hello"; single_line2 = "World"; # Það er hægt að breyta mörgum línum í eina með ; 
+print(single_line1, single_line2)
+
+multi_line1 = ("Hæ \
+Heimur") # Það er hægt að brjóta upp eina línu með \
+print(multi_line1)
